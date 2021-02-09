0dee13e25caa094c80a557d9f022cdc5ce702457
geekadpt <geekadpt@163.com>
2021-02-09 11:13:59 +0000

    请求验证成功

--- app/Http/Controllers/Api/User/VerificationCodesController.php
+++ app/Http/Controllers/Api/User/VerificationCodesController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api\User;
 
+use App\Http\Requests\Api\User\VerificationCodeRequest;
 use Illuminate\Http\Request;
 
 class VerificationCodesController extends Controller
 {
-    public function store()
+    public function store(VerificationCodeRequest $request)
     {
         return response()->json(['test_message' => 'store verification code']);
     }
